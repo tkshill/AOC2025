a82b13bf00f0c65a09a209d1f4130370dbeeb4f4
Kirk <tkshillinz@icloud.com>
2025-12-06 20:49:06 -0700

    final edits

--- day06.hs
+++ day06.hs
@@ -4,36 +4,43 @@
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
-import Data.List (findIndex)
-import Data.Maybe (fromMaybe)
+import Data.List (findIndex, transpose)
+import Data.Maybe (fromMaybe, catMaybes, mapMaybe)
+import Debug.Trace (trace)
+import Text.Read (readMaybe)
 
 type Op = Int -> Int -> Int
+type Chunk = (Op, [Int])
+type Input = [[Char]]
+type NumParser = Int -> [String] -> [Int]
 
-charToOp :: Char -> (Int -> Int -> Int)
-charToOp = \case '+' -> (+); '*' -> (*)
-
-chomper :: [(Op, [Int])] -> [[Char]] -> [(Op, [Int])]
-chomper chomped feed
+chomper :: NumParser -> [Chunk] -> Input -> [Chunk]
+chomper nParser chomped feed
     | opIndex == 0 = chomped
-    | otherwise = chomper ((op, nums) : chomped) $ map (drop (opIndex + 1)) feed
+    | otherwise = chomper nParser ((op, nums) : chomped) $ map (drop (opIndex + 1)) feed
     where
         opIndex = fromMaybe 0 . findIndex (/= ' ') $ last feed
         op = charToOp $ last feed !! opIndex
-        nums = map (read . reverse . take (opIndex + 1)) $ init feed
+        nums = nParser opIndex $ init feed
+        charToOp = \case '+' -> (+); '*' -> (*)
+
+solve :: NumParser -> T.Text -> Int
+solve nParser txt =
+    sum . map (uncurry foldl1) . chomper nParser [] $ map reverse . lines . T.unpack $ txt
 
 solvePart1 :: T.Text -> Int
-solvePart1 txt =
-    sum . map (uncurry foldl1) . chomper [] $ map reverse . lines . T.unpack $ txt
+solvePart1 = solve numParser
+    where
+        numParser idx = map (read . reverse . take (idx + 1))
 
 solvePart2 :: T.Text -> Int
-solvePart2 _ = 0
+solvePart2 = solve numParser
+    where
+        numParser idx = mapMaybe readMaybe . transpose . map (take (idx + 1))
 
 main :: IO ()
 main = do
     args <- getArgs
     raw <- TIO.readFile $ head args
 
-    let printResults p1 p2 = do
-          putStrLn $ "Part 1: " <> show p1 <> "\nPart 2: " <> show p2
-
-    printResults (solvePart1 raw) (solvePart2 raw)
+    putStrLn $ "Part 1: " <> show (solvePart1 raw) <> "\nPart 2: " <> show (solvePart2 raw)
