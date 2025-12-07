3fa641d99e8420d94d3857bce17c6b66b5f1785e
Kirk <tkshillinz@icloud.com>
2025-12-06 21:43:07 -0700

    using unfold

--- day06.hs
+++ day06.hs
@@ -4,28 +4,27 @@
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
-import Data.List (findIndex, transpose)
-import Data.Maybe (fromMaybe, catMaybes, mapMaybe)
-import Debug.Trace (trace)
+import Data.List (findIndex, transpose, unfoldr)
+import Data.Maybe (fromMaybe, mapMaybe)
 import Text.Read (readMaybe)
 
-type Op = Int -> Int -> Int
-type Chunk = (Op, [Int])
-type Input = [[Char]]
+type Operator = Int -> Int -> Int
+type Chunk = (Operator, [Int])
 type NumParser = Int -> [String] -> [Int]
 
-chomper :: NumParser -> [Chunk] -> Input -> [Chunk]
-chomper nParser chomped feed
-    | opIndex == 0 = chomped
-    | otherwise = chomper nParser ((op, nums) : chomped) $ map (drop (opIndex + 1)) feed
+chunker :: NumParser -> [String] -> Maybe (Chunk, [String])
+chunker nParser feed
+    | opIndex == 0 = Nothing
+    | otherwise = Just ((op, nums), rest)
     where
         opIndex = fromMaybe 0 . findIndex (/= ' ') $ last feed
         op = (\case '+' -> (+); '*' -> (*)) $ last feed !! opIndex
         nums = nParser opIndex $ init feed
+        rest = map (drop (opIndex + 1)) feed
 
 solve :: NumParser -> T.Text -> Int
 solve nParser txt =
-    sum . map (uncurry foldl1) . chomper nParser [] $ map reverse . lines . T.unpack $ txt
+    sum . map (uncurry foldl1) . unfoldr (chunker nParser) $ map reverse . lines . T.unpack $ txt
 
 solvePart1 :: T.Text -> Int
 solvePart1 = solve numParser
